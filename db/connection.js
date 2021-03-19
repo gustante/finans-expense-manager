5e3df32562c7241ea13bb523a032d6a653e94566
Gustavo Faria <gustavoff14@gmail.com>
2021-03-19 21:28:46 +0000

    Updates connection string

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 
 let mongoose = require('mongoose');
+
 let mongoDB = process.env.MONGODBURI;
-mongoose.connect(mongoDB.toString(), { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true });
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology:true });
 let connection = mongoose.connection;
 connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
