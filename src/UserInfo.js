298f570659f1ecfd825ef185d2fe123594ffc569
Gustavo Faria <gustavoff14@gmail.com>
2022-03-18 06:25:16 +0000

    fix: deletes comma separating first name and last time

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -40,7 +40,7 @@ class UserInfo extends React.Component {
 
                             <ul className="list-group ">
 
-                                <li className="list-group-item "><span className="font-weight-bold">Name:</span> {firstName},{lastName}</li>
+                                <li className="list-group-item "><span className="font-weight-bold">Name:</span> {firstName} {lastName}</li>
                                 <li className="list-group-item"><span className="font-weight-bold">Email:</span> {email}</li>
                                 <li className="list-group-item"><span className="font-weight-bold">Phone number:</span> {phoneNumber}</li>
                                 <li className="list-group-item"><span className="font-weight-bold">Password:</span> ******</li>
