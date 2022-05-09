bd900f315239f4cd7fc38e14be623d319e0490a5
Gustavo Faria <gustavoff14@gmail.com>
2022-05-09 01:13:38 +0000

    disables Google Login button

--- src/Login.js
+++ src/Login.js
@@ -37,7 +37,7 @@ class Login extends React.Component {
                     </form>
 
                     <div className="form-signin mb-5 text-center">
-                        <button className="btn btn-lg btn-success btn-block" onClick={this.props.handleGoogleLogIn}>Sign in with <i className="fab fa-google"></i></button>
+                        <button className="btn btn-lg btn-success btn-block disabled" onClick={this.props.handleGoogleLogIn}>Sign in with <i className="fab fa-google"></i></button>
                         <Link className="btn text-primary" to="/register">
                             or create an account
                         </Link>
