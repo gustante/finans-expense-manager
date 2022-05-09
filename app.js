356870b399dca22ba2689fe2ae0eb9decc57c6dd
Gustavo Faria <gustavoff14@gmail.com>
2022-05-09 01:09:10 +0000

    fix: changes express.static to serve files in public folder by absolute path. Was causing the error 'CANNOT GET' after deploying to AWS instance

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const router = require("./routes/index.js")
 const session = require('express-session');
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+const path = require('path')
+app.use( express.static(path.join(__dirname, 'public')))
+
 app.use(express.json());
 
 app.use(
