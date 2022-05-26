ab098bf01b9a939a63f98819171a70df3e50d8fc
Gustavo <53026187+gustante@users.noreply.github.com>
2022-05-26 00:05:55 -0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,25 @@ const router = require("./routes/index.js")
 const session = require('express-session');
 const passport = require('passport');
 
-var auth = require('./passport');
+const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIEND_ID,
+    clientSecret: process.env.GOOGLE_CLOUD_SECRET,
+    callbackURL: "https://finans-prpwmfaewa-uw.a.run.app/api/v1.0/oauth/google/callback",
+    passReqToCallback: true
+  },
+ function(request, accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }));
+
+  passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
